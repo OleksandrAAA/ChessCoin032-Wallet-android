d2465c18d668de8acb100f4531db970333f0c89f
OleksandrAAA <aleksandkolesnik73@gmail.com>
2022-05-17 07:54:57 +0700

    Modified a URL for groundcontrol service.

--- blue_modules/constants.js
+++ blue_modules/constants.js
@@ -2,4 +2,5 @@
  * Let's keep config vars, constants and definitions here
  */
 
-export const groundControlUri = 'https://groundcontrol-bluewallet.herokuapp.com/';
+//export const groundControlUri = 'https://groundcontrol-bluewallet.herokuapp.com/';
+export const groundControlUri = 'http://54.36.163.33:8001/';
